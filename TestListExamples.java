2bc919286c82df10215fcfc9c491311fafaf826d
Joe Politz <joe@cs.brown.edu>
2022-11-11 14:05:09 -0800

    timeout example

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,5 +2,9 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestListExamples {
+  @Test(timeout = 100)
+  public void testTimeout() {
+    while(true) {}
+  }
   // Write your grading tests here!
 }
